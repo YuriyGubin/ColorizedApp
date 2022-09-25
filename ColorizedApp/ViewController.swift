19da98d1ad38a3d6736a6d4d2dbd03bcd54f833b
YuriyGubin <iosdevelop.2022@gmail.com>
2022-09-25 16:47:27 +0500

    ViewController

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -24,11 +24,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        setupViews()
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    
+    @IBAction func redSliderAction() {
+        redValueLabel.text = "\(round(redSlider.value * 100) / 100)"
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = "\(round(greenSlider.value * 100) / 100)"
+    }
+    
+    
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = "\(round(blueSlider.value * 100) / 100)"
+    }
+    
     private func setupViews() {
         colorizedView.layer.cornerRadius = 10
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        blueSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
         
         
     }
