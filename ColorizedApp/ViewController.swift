8289748f5a8ef32445e97ad77fd0f3f25e219760
GubinYuriy <iosdevelop.2022@gmail.com>
2022-10-11 12:24:45 +0500

    Fix HW

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -16,45 +16,52 @@ class ViewController: UIViewController {
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLabel: UILabel!
     
-    @IBOutlet var redSlider: UISlider!
-    @IBOutlet var greenSlider: UISlider!
-    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var redSlider: UISlider! {
+        didSet {
+            redSlider.minimumTrackTintColor = .red
+            redSlider.maximumTrackTintColor = .red.withAlphaComponent(0.2)
+        }
+    }
+    @IBOutlet var greenSlider: UISlider! {
+        didSet {
+            greenSlider.minimumTrackTintColor = .green
+            greenSlider.maximumTrackTintColor = .green.withAlphaComponent(0.2)
+        }
+    }
+    @IBOutlet var blueSlider: UISlider! {
+        didSet {
+            blueSlider.minimumTrackTintColor = .blue
+            blueSlider.maximumTrackTintColor = .blue.withAlphaComponent(0.2)
+        }
+    }
     
     // MARK: - methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorizedView.layer.cornerRadius = 15
         
-        setupViews()
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
     
     
     // MARK: - IBActions
     
     @IBAction func slidersAction(_ sender: UISlider) {
-        colorizedView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
         redValueLabel.text = String(format: "%.02f", redSlider.value)
         greenValueLabel.text = String(format: "%.02f", greenSlider.value)
         blueValueLabel.text = String(format: "%.02f", blueSlider.value)
-    }
-    
-    
-    // MARK: - private methods
-    
-    private func setupViews() {
-        colorizedView.layer.cornerRadius = 10
-        
-        redSlider.value = 0
-        redSlider.minimumTrackTintColor = .red
-        redSlider.maximumTrackTintColor = .red.withAlphaComponent(0.2)
-        
-        greenSlider.value = 0
-        greenSlider.minimumTrackTintColor = .green
-        greenSlider.maximumTrackTintColor = .green.withAlphaComponent(0.2)
-        
-        blueSlider.value = 0
-        blueSlider.minimumTrackTintColor = .blue
-        blueSlider.maximumTrackTintColor = .blue.withAlphaComponent(0.2)
     }
 }
 
